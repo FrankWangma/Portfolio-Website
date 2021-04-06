816ed10bb23578d7648357f93deca66680c7f8be
Frank Wang Ma <frankwangma@gmail.com>
2021-04-06 21:49:23 +1200

    Added hidden Drawers

--- portfolio-website/src/Components/Sidebar.js
+++ portfolio-website/src/Components/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Drawer, List, ListItem, Typography} from '@material-ui/core';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Drawer, Hidden, List, ListItem, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { SocialIcon  } from 'react-social-icons'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+
     MuiDrawer: {
         backgroundColor: "#2b2b2b",
         width: 240,
@@ -14,38 +14,57 @@ const useStyles = makeStyles({
     Text: {
         color: "#ffffff"
     },
-});
+}));
+
+
 
 const Sidebar = (props) => {
     const classes = useStyles();
       
+    const drawerContent = (
+        <List >
+            <ListItem> 
+                <Typography classes={{root: classes.Text}} variant="h5">Frank Wang Ma</Typography>
+            </ListItem>
+            {props.SidebarNames.map((text, index) => (
+                <Link to={props.Links[index]}>
+                    <ListItem button key={text}>
+                        <Typography classes={{root: classes.Text}}>{text}</Typography>
+                    </ListItem>
+                </Link>
+            ))}
+            <ListItem>
+                <SocialIcon url="https://www.linkedin.com/in/frank-wangma/" bgColor="#ffffff"/>
+            </ListItem>
+            <ListItem>
+                <SocialIcon url="https://github.com/FrankWangma" bgColor="#ffffff"/>
+            </ListItem>
+            <ListItem>
+                <SocialIcon url="https://www.facebook.com/frank.wangma.5/" bgColor="#ffffff"/>
+            </ListItem>
+        </List>
+    )
+
     return (
-        <Drawer
-            variant="permanent"
-            classes={{paper: classes.MuiDrawer}}
-        >
-            <List >
-                <ListItem> 
-                    <Typography classes={{root: classes.Text}} variant="h5">Frank Wang Ma</Typography>
-                </ListItem>
-                {props.SidebarNames.map((text, index) => (
-                    <Link to={props.Links[index]}>
-                        <ListItem button key={text}>
-                            <Typography classes={{root: classes.Text}}>{text}</Typography>
-                        </ListItem>
-                    </Link>
-                ))}
-                <ListItem>
-                    <SocialIcon url="https://www.linkedin.com/in/frank-wangma/" bgColor="#ffffff"/>
-                </ListItem>
-                <ListItem>
-                    <SocialIcon url="https://github.com/FrankWangma" bgColor="#ffffff"/>
-                </ListItem>
-                <ListItem>
-                    <SocialIcon url="https://www.facebook.com/frank.wangma.5/" bgColor="#ffffff"/>
-                </ListItem>
-            </List>
-        </Drawer>
+        <div>
+            <Hidden smUp implementation="css">
+                <Drawer
+                    variant='temporary'
+                    classes={{paper: classes.MuiDrawer}}
+                    anchor='left'
+                >
+                    {drawerContent}
+                </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+                <Drawer
+                    variant='permanent'
+                    classes={{paper: classes.MuiDrawer}}
+                >
+                    {drawerContent}
+                </Drawer>
+            </Hidden>
+        </div>
     )
 }
 
