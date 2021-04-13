44478b08df0ad0983972fce7e4d96db07016dabe
Frank Wang Ma <frankwangma@gmail.com>
2021-04-13 21:31:40 +1200

    Added info about projects from the API

--- portfolio-website/src/Pages/Projects.js
+++ portfolio-website/src/Pages/Projects.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Card, CardActionArea, CardHeader, CardMedia, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom';
 
-const Projects = () => {
-    return (
+const Projects = ({user}) => {
+    return ( 
         <div>
-            Projects
+            <Typography variant='h3'>My Projects</Typography>
+            <div>
+                {user.projects.map(project => (
+                    <a target='_blank' rel="noreferrer noopener" href={project.githubUrl}>
+                        <Card>
+                            {console.log(project)}
+                            <CardActionArea>
+                                <CardHeader title={project.name} />
+                                <CardMedia>
+                                    <img src={project.images[0].resolutions.mobile.url} alt={''} />
+                                </CardMedia>
+                            </CardActionArea>
+                        </Card>
+                    </a>
+                ))}
+            </div>
         </div>
     )
 }
