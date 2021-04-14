45f78054a82af49f78f66a4ad4ddb41d415d63d4
Frank Wang Ma <frankwangma@gmail.com>
2021-04-14 20:41:45 +1200

    Added a summary to the Projects, and tweaked the widths and heights of the images

--- portfolio-website/src/Pages/Projects.js
+++ portfolio-website/src/Pages/Projects.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Card, CardActionArea, CardHeader, CardMedia, makeStyles, Typography } from '@material-ui/core'
+import { Card, CardActionArea, CardHeader, CardMedia, CardContent, Grid, makeStyles, Typography } from '@material-ui/core'
 const useStyles = makeStyles((project) => ({
+    parent: {
+        margin: '5%'
+    },
     root: {
-        maxWidth: 600,
+        maxWidth: 500,
+        height: 'auto'
     },
     link: {
         display: 'block',
-        maxWidth: 600,
+        maxWidth: 500,
         textDecoration: 'none'
+    },
+    image: {
+        height: 'auto',
+        maxHeight: '300px',
+        width: 'auto',
+        maxWidth: '500px',
     }
 }));
 
@@ -15,23 +25,37 @@ const Projects = ({user}) => {
     const classes = useStyles();
 
     return ( 
-        <div>
+        <div className={classes.parent}>
             <Typography variant='h3'>My Projects</Typography>
-            <div>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="flex-start"
+            >
                 {user.projects.map(project => (
-                    <a className={classes.link} target='_blank' rel="noreferrer noopener" href={project.githubUrl}>
-                        <Card className={classes.root}>
-                            {console.log(project)}
-                            <CardActionArea>
-                                <CardHeader title={project.name} />
-                                <CardMedia>
-                                    <img src={project.images[0].resolutions.mobile.url} alt={''} />
-                                </CardMedia>
-                            </CardActionArea>
-                        </Card>
-                    </a>
+                    
+                    <Grid item xs
+                    justify="center">
+                        <a className={classes.link} target='_blank' rel="noreferrer noopener" href={project.githubUrl}>
+                            <Card className={classes.root}>
+                                {console.log(project)}
+                                <CardActionArea>
+                                    <CardHeader title={project.name} />
+                                    <CardMedia>
+                                        <img className={classes.image} src={project.images[0].resolutions.mobile.url} alt={''} />
+                                    </CardMedia>
+                                    <CardContent>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            {project.summary}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </a>
+                    </Grid>
                 ))}
-            </div>
+            </Grid>
         </div>
     )
 }
