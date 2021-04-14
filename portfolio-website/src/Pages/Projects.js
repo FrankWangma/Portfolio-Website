99d2fd44b9206c5af2c0d38002f4bcb07f56aa43
Frank Wang Ma <frankwangma@gmail.com>
2021-04-14 14:50:04 +1200

    Added some styling for the Project Cards

--- portfolio-website/src/Pages/Projects.js
+++ portfolio-website/src/Pages/Projects.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Card, CardActionArea, CardHeader, CardMedia, Typography } from '@material-ui/core'
-import { Link } from 'react-router-dom';
+import { Card, CardActionArea, CardHeader, CardMedia, makeStyles, Typography } from '@material-ui/core'
+const useStyles = makeStyles((project) => ({
+    root: {
+        maxWidth: 600,
+    },
+    link: {
+        display: 'block',
+        maxWidth: 600,
+        textDecoration: 'none'
+    }
+}));
 
 const Projects = ({user}) => {
+    const classes = useStyles();
+
     return ( 
         <div>
             <Typography variant='h3'>My Projects</Typography>
             <div>
                 {user.projects.map(project => (
-                    <a target='_blank' rel="noreferrer noopener" href={project.githubUrl}>
-                        <Card>
+                    <a className={classes.link} target='_blank' rel="noreferrer noopener" href={project.githubUrl}>
+                        <Card className={classes.root}>
                             {console.log(project)}
                             <CardActionArea>
                                 <CardHeader title={project.name} />
